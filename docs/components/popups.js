7a95d0262b66b26d9d0611721449dc7e880d426b
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-09-17 22:40:51 +0300

    Build

---
+++ docs/components/popups.js
@@ -0,0 +1,60 @@
+document.addEventListener('click', (e) => {
+  const button = e.target.closest('[data-popup-button]');
+
+  if (!button) return true;
+
+  e.preventDefault();
+
+  const name = button.dataset.popupButton;
+
+  const backdrop = document.querySelector('[data-popup-backdrop]');
+  const popup = document.querySelector(`[data-popup-name="${name}"]`);
+
+  if (!popup || !backdrop) return true;
+
+  const popups = document.querySelectorAll('[data-popup-name]:not([hidden])');
+
+  for (const current of popups) {
+    current.hidden = true;
+  }
+
+  backdrop.hidden = false;
+  popup.hidden = false;
+});
+
+function closePopup() {
+  const backdrop = document.querySelector('[data-popup-backdrop]');
+
+  if (!backdrop) return true;
+
+  const popups = document.querySelectorAll('[data-popup-name]:not([hidden])');
+
+  for (const current of popups) {
+    current.hidden = true;
+  }
+
+  backdrop.hidden = true;
+}
+
+document.addEventListener('click', (e) => {
+  const backdrop = e.target.closest('[data-popup-backdrop]');
+  const popup = e.target.closest('[data-popup-name]');
+
+  if (!backdrop || popup) return true;
+
+  closePopup();
+});
+
+document.addEventListener('click', (e) => {
+  const button = e.target.closest('[data-popup-close]');
+
+  if (!button) return true;
+
+  closePopup();
+});
+
+document.addEventListener('keydown', function (e) {
+  if ('Escape' !== e.key) return true;
+
+  closePopup();
+});
