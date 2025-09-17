4c95ea7feece153c1649cd595feb32deb62cec42
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-09-17 22:39:29 +0300

    Refactor footer login button and popup system

--- public/components/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', (e) => {
-  const button = e.target.closest('.button-login-footer');
-
-  if (!button) return true;
-
-  e.preventDefault();
-
-  console.log('click', button);
-});
