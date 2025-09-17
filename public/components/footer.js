74d4b5007cdeb5486bdbe31d0febd8aaa243c92c
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-09-17 21:19:25 +0300

    Refactor popup system and layout structure

--- public/components/footer.js
+++ public/components/footer.js
@@ -1,14 +1,4 @@
 document.addEventListener('click', (e) => {
-  const button = e.target.closest('.button-menu-footer');
-
-  if (!button) return true;
-
-  e.preventDefault();
-
-  console.log('click', button);
-});
-
-document.addEventListener('click', (e) => {
   const button = e.target.closest('.button-login-footer');
 
   if (!button) return true;
