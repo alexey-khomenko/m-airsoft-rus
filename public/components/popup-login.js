332db012e27675102aaf4e97df728c337053b63a
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-09-23 00:20:21 +0300

    Add profile page and refactor login system

--- public/components/popup-login.js
+++ public/components/popup-login.js
@@ -1,24 +1,86 @@
-document.addEventListener('click', (e) => {
-  const step1 = e.target.closest('[data-popup-login-step="1"]');
+function resetError(input) {
+  input.classList.remove('util-input-error');
+}
 
-  if (!step1) return true;
 
-  const step2 = step1.closest('.popup-login').querySelector('[data-popup-login-step="2"]');
+document.addEventListener('focusin', (e) => {
+  const input = e.target.closest('[data-form-login-phone] [name="phone"]');
 
-  if (!step2) return true;
+  if (!input) return true;
+
+  resetError(input);
+});
+
+document.addEventListener('submit', (e) => {
+  const form = e.target.closest('[data-form-login-phone]');
+
+  if (!form) return true;
+
+  e.preventDefault();
+
+  const action = form.action;
+  const input = form.querySelector('[name="phone"]');
+  const value = input.value.trim();
+
+  if (0 === value.length) {
+    input.classList.add('util-input-error');
+    return true;
+  }
+
+  console.log('submit', action, value);
+
+  const step1 = document.querySelector('[data-popup-login-step="1"]');
+  const step2 = document.querySelector('[data-popup-login-step="2"]');
 
   step1.hidden = true;
   step2.hidden = false;
 });
 
 document.addEventListener('click', (e) => {
-  const step2 = e.target.closest('[data-popup-login-step="2"]');
+  const button = e.target.closest('[data-popup-login-cancel]');
 
-  if (!step2) return true;
+  if (!button) return true;
 
-  const step1 = step2.closest('.popup-login').querySelector('[data-popup-login-step="1"]');
+  window.closePopup();
+});
 
-  if (!step1) return true;
+
+document.addEventListener('focusin', (e) => {
+  const input = e.target.closest('[data-form-login-code] [name="code"]');
+
+  if (!input) return true;
+
+  input.classList.remove('util-input-error');
+});
+
+document.addEventListener('submit', (e) => {
+  const form = e.target.closest('[data-form-login-code]');
+
+  if (!form) return true;
+
+  e.preventDefault();
+
+  const action = form.action;
+  const input = form.querySelector('[name="code"]');
+  const value = input.value.trim();
+
+  if (+input.maxLength !== value.length) {
+    input.classList.add('util-input-error');
+    return true;
+  }
+
+  console.log('submit', action, value);
+
+  window.location.reload();
+});
+
+document.addEventListener('click', (e) => {
+  const button = e.target.closest('[data-popup-login-back]');
+
+  if (!button) return true;
+
+  const step1 = document.querySelector('[data-popup-login-step="1"]');
+  const step2 = document.querySelector('[data-popup-login-step="2"]');
 
   step2.hidden = true;
   step1.hidden = false;
@@ -30,5 +92,13 @@ document.addEventListener('click', (e) => {
 
   if (!button) return true;
 
-  console.log('Another login'); // TODO: popup login
+  console.log('Another login');
+});
+
+document.addEventListener('click', (e) => {
+  const button = e.target.closest('[data-popup-login-yandex]');
+
+  if (!button) return true;
+
+  console.log('Yandex ID');
 });
