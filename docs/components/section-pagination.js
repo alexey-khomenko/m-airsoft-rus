98e8bf4e3017966ee13a8e9445683fc3375966be
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-10-18 00:46:18 +0300

    Build

--- docs/components/section-pagination.js
+++ docs/components/section-pagination.js
@@ -1,1 +1,22 @@
-console.log('section-pagination');
+window.sectionPaginationJsIsLoaded = false;
+
+window.addEventListener('load', () => {
+  if (window.sectionPaginationJsIsLoaded) return true;
+
+  window.sectionPaginationJsIsLoaded = true;
+
+  document.addEventListener('click', (e) => {
+    const linkA = e.target.closest('.section-pagination .page.disabled');
+    const linkB = e.target.closest('.section-pagination .page.current');
+
+    if (linkA || linkB) e.preventDefault();
+  });
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('.section-pagination .show-more');
+
+    if (!button) return true;
+
+    console.log(button);
+  });
+});
