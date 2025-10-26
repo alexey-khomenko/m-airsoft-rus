fee32e296646aa2a19ecbab806f155bf45fbe81f
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-10-26 21:24:06 +0200

    Build

--- docs/pages/profile.js
+++ docs/pages/profile.js
@@ -3,7 +3,7 @@ document.addEventListener('click', (e) => {
 
   if (!button) return true;
 
-  console.log('POST request to logout'); // TODO: implement logout
+  console.log('POST request to logout');
 
   window.location.assign(button.dataset.buttonLogoutTo);
 });
