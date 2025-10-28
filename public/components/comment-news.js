ef0b99e75f6b48a814db1a83aef8897e8100c9c8
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-10-28 23:03:02 +0200

    Refactor comments

--- public/components/comment-news.js
+++ public/components/comment-news.js
@@ -1,3 +1,36 @@
-// TODO component CommentNews
-//  Клик Имя
-//  Клик Цитировать
+window.commentNewsJsIsLoaded = false;
+
+window.addEventListener('load', () => {
+  if (window.commentNewsJsIsLoaded) return true;
+
+  window.commentNewsJsIsLoaded = true;
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('[data-comment-btn-name]');
+
+    if (!button) return true;
+
+    const comment = button.closest('[data-comment-id]');
+    const name = comment.querySelector('[data-comment-name]').textContent.trim();
+
+    console.log('comment name', name);
+
+    const textarea = document.querySelector('#text');
+    const oldValue = textarea.value.trim();
+
+    textarea.value = 0 < oldValue.length ? `${oldValue}\n${name}, ` : `${name}, `;
+
+    textarea.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
+  });
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('[data-comment-btn-text]');
+
+    if (!button) return true;
+
+    const comment = button.closest('[data-comment-id]');
+    const text = comment.querySelector('[data-comment-text]').innerHTML.trim();
+
+    console.log('comment text', text);
+  });
+});
