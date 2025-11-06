bf5dbe0f0c012578ccdf4f6f9a00c46520277a22
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-11-07 01:57:50 +0200

    Build

---
+++ docs/components/tile-order.js
@@ -0,0 +1,51 @@
+window.tileOrderJsIsLoaded = false;
+
+window.addEventListener('load', () => {
+  if (window.tileOrderJsIsLoaded) return true;
+
+  window.tileOrderJsIsLoaded = true;
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('[data-button-pay]');
+
+    if (!button) return true;
+
+    console.log('pay');
+  });
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('[data-payment-id-new]');
+
+    if (!button) return true;
+
+    const paymentId = +button.dataset.paymentIdNew;
+    const orderId = +button.closest('[data-order-id]').dataset.orderId;
+
+    console.log('change', orderId, paymentId);
+
+    window.location.reload();
+  });
+
+  document.addEventListener('click', (e) => {
+    const button = e.target.closest('[data-change-payment]');
+
+    if (!button) return true;
+
+    const backdrop = document.querySelector('[data-payments-backdrop]');
+    const payments = backdrop.querySelector('[data-payments]');
+
+    payments.setAttribute('data-order-id', button.dataset.orderId);
+    backdrop.hidden = false;
+  });
+
+  document.addEventListener('click', (e) => {
+    const backdrop = e.target.closest('[data-payments-backdrop]');
+    const payments = e.target.closest('[data-payments]');
+
+    if (!backdrop || payments) return true;
+
+    document.querySelector('[data-payments-backdrop]').hidden = true;
+  });
+
+  // TODO Скрытие крестиком
+});
