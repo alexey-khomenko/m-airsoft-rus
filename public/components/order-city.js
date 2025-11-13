0db1fc54968ee14ab2abb805864afb297eb7c928
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-11-14 01:01:57 +0200

    Add city input and refactor personal form fields

---
+++ public/components/order-city.js
@@ -0,0 +1,48 @@
+window.orderCityJsIsLoaded = false;
+
+window.addEventListener('load', () => {
+  if (window.orderCityJsIsLoaded) return true;
+
+  window.orderCityJsIsLoaded = true;
+
+  const component = {
+    form: document.querySelector('[data-form-order-city]'),
+    limit: 3,
+    debounceTimer: null,
+  };
+
+  if (!component.form) console.log('[data-form-order-city] not found');
+
+
+  document.addEventListener('submit', (e) => {
+    const form = e.target.closest('[data-form-order-city]');
+
+    if (form) e.preventDefault();
+  });
+
+
+  document.addEventListener('input', (e) => {
+    const input = e.target.closest('[data-input-order-city]');
+
+    if (!input) return true;
+
+    clearTimeout(component.debounceTimer);
+
+    const action = component.form.action;
+    const value = input.value.trim();
+
+    if (component.limit > value.length) return true;
+
+    component.debounceTimer = setTimeout(() => {
+
+
+      console.log('POST request to', action);
+      console.log('city', value);
+      const responseCity = value;
+      console.log('responseCity', responseCity);
+
+
+      input.value = responseCity;
+    }, 1000);
+  });
+});
