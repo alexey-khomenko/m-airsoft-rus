83a3f8d6957c6753d40c8a8124fdd7c64e61059a
alexey <alexey.alexeevich.khomenko@gmail.com>
2025-11-18 01:11:28 +0200

    Build

--- docs/components/order-delivery.js
+++ docs/components/order-delivery.js
@@ -5,15 +5,69 @@ window.addEventListener('load', () => {
 
   window.orderDeliveryJsIsLoaded = true;
 
+  window.previousDeliveryId = null;
+
+  if ('undefined' === typeof window.deliveries) window.deliveries = {};
+
+  function mount(id) {
+    if (!id) return;
+
+    if ('undefined' === typeof window.deliveries[`delivery${id}`]) {
+      console.log(`Script for 'delivery${id}' not found`);
+      return;
+    }
+
+    if ('undefined' === typeof window.deliveries[`delivery${id}`].mount) {
+      console.log(`Function 'delivery${id}.mount()' not found`);
+      return;
+    }
+
+    const delivery = document.querySelector(`.order-delivery-${id}`);
+    if (delivery) delivery.hidden = false;
+
+    window.deliveries[`delivery${id}`].mount();
+  }
+
+  function unmount(id) {
+    if (!id) return;
+
+    if ('undefined' === typeof window.deliveries[`delivery${id}`]) {
+      console.log(`Script for 'delivery${id}' not found`);
+      return;
+    }
+
+    if ('undefined' === typeof window.deliveries[`delivery${id}`].unmount) {
+      console.log(`Function 'delivery${id}.unmount()' not found`);
+      return;
+    }
+
+    const delivery = document.querySelector(`.order-delivery-${id}`);
+    if (delivery) delivery.hidden = true;
+
+    window.deliveries[`delivery${id}`].unmount();
+  }
+
   document.addEventListener('changeOrderCity', () => {
     const form = document.querySelector(`[data-order-delivery-reload-action]`);
+    const formGrid = form.querySelector('.form-grid');
     const action = form.dataset.orderDeliveryReloadAction;
 
     form.reset();
-    form.querySelector('[data-tile-order-grid-edit]').hidden = true;
+    formGrid.querySelector('[data-tile-order-grid-edit]').hidden = true;
 
-    const tiles = form.querySelectorAll(`[data-tile-order-grid]`);
+    const tiles = formGrid.querySelectorAll(`[data-tile-order-grid]`);
 
+
+    const responseDeliveries = [];
+    for (const tile of tiles) {
+      const deliveryId = tile.querySelector('[type="radio"]').value;
+      const delivery = form.querySelector(`.order-delivery-${deliveryId}`);
+
+      if (!delivery) continue;
+
+      responseDeliveries.push(delivery.cloneNode(true));
+      break;
+    }
 
     const responseTiles = [];
     for (const tile of tiles) {
@@ -22,34 +76,44 @@ window.addEventListener('load', () => {
     }
 
 
-    for (const tile of tiles) tile.remove();
+    for (const tile of tiles) {
+      const deliveryId = tile.querySelector('[type="radio"]').value;
+      const delivery = form.querySelector(`.order-delivery-${deliveryId}`);
+
+      if (delivery) {
+        unmount(deliveryId);
+        form.querySelector(`.order-delivery-${deliveryId}`).remove();
+      }
+
+      tile.remove();
+    }
 
 
     console.log('POST request to', action);
 
 
     responseTiles.reverse();
-    for (const tile of responseTiles) form.prepend(tile);
+    for (const tile of responseTiles) formGrid.prepend(tile);
+
+    for (const delivery of responseDeliveries) form.append(delivery);
 
     form.dispatchEvent(new CustomEvent('checkOrderSummary', {bubbles: true}));
   });
+
+  const form = document.querySelector(`[data-form-order-delivery]`);
+  form.reset();
 
   document.addEventListener('input', (e) => {
     const radio = e.target.closest('[name="delivery"]');
 
     if (!radio) return true;
 
-    const form = document.querySelector(`[data-form-order-delivery]`);
-    const action = form.action;
     const value = radio.value;
 
+    unmount(window.previousDeliveryId);
+    mount(value);
 
-    console.log('POST request to', action);
-    console.log('delivery', value);
-
-
+    window.previousDeliveryId = value;
     form.dispatchEvent(new CustomEvent('checkOrderSummary', {bubbles: true}));
   });
-
-  // TODO Дополнительные поля доставки
 });
