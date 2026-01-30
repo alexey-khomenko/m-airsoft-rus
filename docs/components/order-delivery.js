b0a000f600658f3d68a173a0f15fb4423d5d9abb
alexey <alexey.alexeevich.khomenko@gmail.com>
2026-01-31 01:19:22 +0200

    build

--- docs/components/order-delivery.js
+++ docs/components/order-delivery.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
 
   window.orderDeliveryJsIsLoaded = true;
 
+  // TODO:
   window.previousDeliveryId = null;
 
   if ('undefined' === typeof window.deliveries) window.deliveries = {};
@@ -96,31 +97,157 @@ window.addEventListener('load', () => {
     for (const tile of responseTiles) formGrid.prepend(tile);
 
     for (const delivery of responseDeliveries) form.append(delivery);
-
-    form.dispatchEvent(new CustomEvent('checkOrderSummary', {bubbles: true})); // TODO: -
   });
+
+
+  function buildTiles(deliveries, deliveryId) {
+    form.hidden = true;
+    form.reset();
+    form.querySelector('[data-tile-order-grid-edit]').hidden = true;
+    deliveries.reverse();
+
+    const sample = document.querySelector(`[data-tile-sample="delivery"] [data-tile-order-grid]`);
+
+    const tiles = form.querySelectorAll(`.form-grid [data-tile-order-grid]`);
+    for (const tile of tiles) tile.remove();
+
+
+    for (const delivery of deliveries) {
+      const tile = sample.cloneNode(true);
+
+      tile.hidden = false;
+
+      tile.querySelector('[type="radio"]').setAttribute('value', delivery.id);
+      tile.querySelector('[type="radio"]').setAttribute('aria-label', delivery.title);
+      tile.querySelector('.img').setAttribute('src', delivery.img);
+      tile.querySelector('.img-checked').setAttribute('src', delivery.imgChecked);
+      tile.querySelector('.name').textContent = delivery.title;
+      tile.querySelector('.info').innerHTML = delivery.info;
+      tile.querySelector('[data-price]').textContent = `${delivery.price} руб.`;
+      tile.querySelector('[data-days]').textContent = delivery.days;
+
+      form.querySelector('.form-grid').prepend(tile);
+    }
+
+
+    if (0 < deliveryId) {
+      const radio = form.querySelector(`[name="delivery"][value="${deliveryId}"]`);
+
+      if (radio) radio.checked = true;
+    }
+
+
+    const tile = form.querySelector('.tile-order-grid:has([type="radio"]:checked)');
+
+    if (tile) tile.click();
+
+    form.hidden = false;
+  }
 
   const form = document.querySelector(`[data-form-order-delivery]`);
 
-  (() => {
-    form.reset();
+  setTimeout(() => {
+    buildTiles(JSON.parse(form.dataset.deliveries), form.dataset.deliveryId);
+  }, 10);
 
-    const tile = form.querySelector('.tile-order-grid:has([type="radio"][checked])');
+  document.addEventListener('updateOrderInfo', async (e) => {
+    if ('deliveries' in e.detail && 'deliveryId' in e.detail) {
+      buildTiles(e.detail.deliveries, e.detail.deliveryId);
+    }
+    else {
+      console.info('info.deliveries not found');
+      console.info('info.deliveryId not found');
+    }
+  });
 
-    if (tile) tile.click();
-  })();
-
-  document.addEventListener('input', (e) => {
+  document.addEventListener('input', async (e) => {
     const radio = e.target.closest('[name="delivery"]');
 
     if (!radio) return true;
 
+    const action = form.action;
     const value = radio.value;
 
-    unmount(window.previousDeliveryId);
-    mount(value);
+    radio.dispatchEvent(new CustomEvent('orderRequestSent', {bubbles: true}));
 
-    window.previousDeliveryId = value;
-    form.dispatchEvent(new CustomEvent('checkOrderSummary', {bubbles: true})); // TODO: -
+    console.log('POST request to', action);
+    console.log('delivery', value);
+
+
+    await new Promise(r => setTimeout(r, 3000));
+    const response = {
+      'info': {
+        deliveries: [
+          {
+            'id': 1,
+            'title': '_Самовывоз',
+            'img': './content/order-delivery-1.png',
+            'imgChecked': './content/order-delivery-1-b.png',
+            'price': 0,
+            'days': '1-4 дня',
+            'info': 'Самовывоз означает, что вы можете забрать свой заказ из нашего магазина в течение 3х рабочих дней после подтверждения заказа менеджером интернет-магазина.',
+          },
+          {
+            'id': 2,
+            'title': '_Самовывоз',
+            'img': './content/order-delivery-2.png',
+            'imgChecked': './content/order-delivery-2.png',
+            'price': 300,
+            'days': '1-4 дня',
+            'info': '...',
+          },
+          {
+            'id': 3,
+            'title': '_Самовывоз',
+            'img': './content/order-delivery-3.png',
+            'imgChecked': './content/order-delivery-3.png',
+            'price': 300,
+            'days': '1-4 дня',
+            'info': '...',
+          },
+          {
+            'id': 4,
+            'title': '_Самовывоз',
+            'img': './content/order-delivery-4.png',
+            'imgChecked': './content/order-delivery-4.png',
+            'price': 300,
+            'days': '1-4 дня',
+            'info': '...',
+          },
+          {
+            'id': 5,
+            'title': '_Самовывоз',
+            'img': './content/order-delivery-5.png',
+            'imgChecked': './content/order-delivery-5.png',
+            'price': 300,
+            'days': '1-4 дня',
+            'info': '...',
+          },
+        ],
+        deliveryId: value,
+        certificate: 'XXXX-XXXX-XXXX',
+        balance: 940,
+        bonuses: 10,
+        old: 8501,
+        discount: 3001,
+        delivery: 301,
+        total: 15031,
+      },
+    };
+
+
+    if ('info' in response) {
+      form.dispatchEvent(new CustomEvent('updateOrderInfo', {bubbles: true, detail: response.info}));
+    }
+    else {
+      console.info('response.info not found');
+    }
+
+    form.dispatchEvent(new CustomEvent('orderRequestReceived', {bubbles: true}));
+
+    // TODO:
+    //   unmount(window.previousDeliveryId);
+    //   mount(value);
+    //   window.previousDeliveryId = value;
   });
 });
