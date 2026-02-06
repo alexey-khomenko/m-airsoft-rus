348abf6427bf2a472e6fad4d50ce51afe91218ec
alexey <alexey.alexeevich.khomenko@gmail.com>
2026-02-06 02:18:41 +0200

    Fix delivery mount on delivery update

--- public/components/order-delivery.js
+++ public/components/order-delivery.js
@@ -84,6 +84,7 @@ window.addEventListener('load', () => {
     if ('deliveries' in e.detail && 'deliveryId' in e.detail) {
       deliveryUnmount();
       await buildTiles(e.detail.deliveries, e.detail.deliveryId);
+      await deliveryMount(e.detail.deliveryId);
     }
     else {
       console.info('info.deliveries not found');
